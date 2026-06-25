98b849898b61bddb5f2c3622dbe2e30387c5dbb1
Yan Araujo <Yanthedev@gmail.com>
2026-06-25 09:45:56 -0300

    Estruturação da classe Emprestimo, com seu contrutor e seus getters e setters

--- rocketseat/library_system/Emprestimo.java
+++ rocketseat/library_system/Emprestimo.java
@@ -1,5 +1,46 @@
 package rocketseat.library_system;
 
+import java.math.BigInteger;
+import java.time.LocalDate;
+
 public class Emprestimo {
+    private BigInteger id;
+    private Livro livro;
+    private String nomeCliente;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(BigInteger id, Livro livro, String nomeCliente, LocalDate dataEmprestimo,
+            LocalDate dataDevolucao) {
+        this.id = id;
+        this.livro = livro;
+        this.nomeCliente = nomeCliente;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
 
 }
